86870ddf47fef5d514ca8d050862a2841e1913e0
MaximKravets <egoregor2021@inbox.ru>
2021-11-11 14:27:30 +1000

    commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+from random import randint
 from pygame.constants import KEYDOWN
 import spritesheet
 
@@ -9,10 +9,49 @@ def show_score(score):
     score_surface = font.render(f"Score: {score}",True,'Red')
     score_rect = score_surface.get_rect(topleft = (0,0))
     screen.blit(score_surface,score_rect)
+
 def show_health(health):
     health_surface = font.render(f"Lives: {health}",True,'Red')
     health_rect = health_surface.get_rect(topright = (width,0))
     screen.blit(health_surface,health_rect)
+
+def move_objects(hearts,rocks,frame_counter):
+    color = (255,0,0)
+    if hearts or rocks:
+        for rock in rocks:
+            rock.y+=4
+            screen.blit(rock_anim[frame_counter%4],rock)
+            pygame.draw.rect(screen,color,rock,2)
+        for heart in hearts:
+            heart.y+=4
+            screen.blit(heart_anim[frame_counter%6],heart)
+            pygame.draw.rect(screen,color,heart,2)
+        hearts = [heart for heart in hearts if heart.y<=600 ]
+        rocks = [rock for rock in rocks if rock.y<=600 ]
+        return hearts,rocks
+    else:
+         return [],[]
+
+def collide_hearts(hearts,player,health):
+    if hearts:
+        for heart in hearts:
+            if player.colliderect(heart):
+                health+=1
+                heart.x = 901
+        if health>5:
+            health =5
+        hearts = [heart for heart in hearts if heart.x!=901]
+    return health,hearts
+def collide_rocks(rocks,player,health):
+    if rocks:
+        for rock in rocks:
+            if player.colliderect(rock):
+                health-=1
+                rock.x =901
+                if health<0:
+                    rocks = [rock for rock in rocks if rock.x!=901]
+                    return -1,rocks
+    return health,rocks
 width = 852
 height = 480
 pygame.init()
@@ -28,6 +67,16 @@ timer = pygame.time.Clock()
 pause_surface = font.render("The game is paused",True,'Red')
 pause_rect = pause_surface.get_rect(center=(width/2,height/2 ))
 # objects 
+pc_surface = pygame.image.load('running.png').convert_alpha()
+pc_sprite_sheet = spritesheet.SpriteSheet(pc_surface)
+pc_anim = []
+pc_x = 400
+for i in range(9):
+    # pc_anim.append(pc_sprite_sheet.get_image(i, 64, 64, 3, (0, 0, 0))) 
+    pc_anim.append(pc_sprite_sheet.get_player(i, 36, 50, 3, (0, 0, 0))) 
+pc_rect = pc_anim[0].get_rect(bottomleft = (pc_x,height))
+print(pc_rect.width)
+
 rock_image = pygame.image.load('rock.png').convert_alpha()
 rock_sprite_sheet = spritesheet.SpriteSheet(rock_image)
 rock_x = 400
@@ -36,6 +85,7 @@ rock_anim = []
 for i in range(4):
     rock_anim.append(rock_sprite_sheet.get_image(i, 32, 32, 3, (0, 0, 0))) 
 rock_rect = rock_anim[0].get_rect(midbottom =(rock_x,rock_y))
+rocks = []
 
 heart_image = pygame.image.load('heart.png').convert_alpha()
 heart_sprite_sheet = spritesheet.SpriteSheet(heart_image)
@@ -43,22 +93,17 @@ heart_x = 300
 heart_y = 250
 heart_anim = []
 for i in range(6):
-    heart_anim.append(heart_sprite_sheet.get_image(i, 64, 64, 3, (0, 0, 0))) 
+    heart_anim.append(heart_sprite_sheet.get_image(i, 64, 64, 1, (0, 0, 0))) 
 heart_rect = heart_anim[0].get_rect(midbottom =(heart_x,heart_y))
+hearts = []
 
-pc_surface = pygame.image.load('running.png').convert_alpha()
-pc_sprite_sheet = spritesheet.SpriteSheet(pc_surface)
-pc_anim = []
-pc_x = 400
-for i in range(9):
-    pc_anim.append(pc_sprite_sheet.get_image(i, 64, 64, 3, (0, 0, 0))) 
-pc_rect = pc_anim[0].get_rect(bottomleft = (pc_x,height))
+
 
 isActive =1
 frame_counter = 0
 
 spawn_timer = pygame.USEREVENT+1
-pygame.time.set_timer(spawn_timer,1200)
+pygame.time.set_timer(spawn_timer,1800)
 
 while True:
     for event in pygame.event.get():
@@ -70,11 +115,17 @@ while True:
                 if event.key ==pygame.K_SPACE:
                     isActive=0  
                 if event.key ==pygame.K_RIGHT:
-                    pc_rect.x+=10
+                    pc_rect.x+=20
                 elif event.key ==pygame.K_LEFT:
                     pc_rect.x-=20
             if event.type == spawn_timer:
-                print('test')                 
+                match randint(0,2):
+                    case 0:
+                        hearts.append(heart_anim[0].get_rect(bottomleft=(randint(0,800),0)))
+                    case 1:
+                        rocks.append(rock_anim[0].get_rect(bottomleft=(randint(0,800),0)))
+                    case 2:
+                        rocks.append(rock_anim[0].get_rect(bottomleft=(randint(0,800),0)))
         else:
             if event.type == pygame.KEYDOWN:
                 if event.key ==pygame.K_SPACE:
@@ -84,19 +135,26 @@ while True:
         show_score(score)
         show_health(health)
         screen.blit(pc_anim[frame_counter%9],pc_rect)
-        rock_rect.y+=4
-        if rock_rect.y>height:
-            rock_rect.y=0
-        screen.blit(rock_anim[frame_counter%4],rock_rect)
+        pygame.draw.rect(screen,(255,0,0),pc_rect,2)
+        hearts,rocks = move_objects(hearts,rocks,frame_counter)
+        health,hearts = collide_hearts(hearts,pc_rect,health)
+        health, rocks = collide_rocks(rocks,pc_rect,health)
+        if health == -1:
+                pygame.quit()
+                sys.exit()
+        # rock_rect.y+=4
+        # if rock_rect.y>height:
+        #     rock_rect.y=0
+        # screen.blit(rock_anim[frame_counter%4],rock_rect)
         frame_counter+=1
         if frame_counter>72:
             frame_counter= 0
-        if pc_rect.colliderect(rock_rect):
-            rock_rect.y =0
-            health-=1
-            if health<0:
-                pygame.quit()
-                sys.exit()
+        # if pc_rect.colliderect(rock_rect):
+        #     rock_rect.y =0
+        #     health-=1
+        #     if health<0:
+        #         pygame.quit()
+        #         sys.exit()
     else:
         screen.blit(pause_surface,pause_rect)
     pygame.display.update()
--- spritesheet.py
+++ spritesheet.py
@@ -11,3 +11,21 @@ class SpriteSheet():
 		image = pygame.transform.scale(image, (width * scale, height * scale))
 		image.set_colorkey(colour)
 		return image
+	def get_player(self, frame, width, height, scale, colour):
+		border = (64 - width)/2
+		vert_offset = 64-height
+		image = pygame.Surface((width, height)).convert_alpha()
+		image.blit(self.sheet, 
+		(0, 0), 
+		((border+frame*(width+2*border)), vert_offset, width, height)
+		)
+		image = pygame.transform.scale(image, (width * scale, height * scale))
+		image.set_colorkey(colour)
+		return image
+	# def get_image(self, frame, width, height, scale, colour, border):
+	# 	image = pygame.Surface((width, height)).convert_alpha()
+	# 	# image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
+	# 	image.blit(self.sheet, (border, border), ((frame * (width+(border*2)), 0, width, height)))
+	# 	image = pygame.transform.scale(image, (width * scale, height * scale))
+	# 	image.set_colorkey(colour)
+	# 	return image
